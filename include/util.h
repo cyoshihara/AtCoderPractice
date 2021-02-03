faa7745d543392435ef718eadee5aabf90080bc1
chryshr <c.y0sh1hara.jp@gmail.com>
2021-02-03 20:27:11 +0900

    190C少し変更

--- include/util.h
+++ include/util.h
@@ -1,13 +1,12 @@
 #include <bits/stdc++.h>
 
 struct util {
-  ///////////////////////////////
   template <typename T>
   static void print(const T& item, const std::string& title = "") {
     if (!title.empty()) print_title(title);
     std::cout << item << std::endl;
   }
-  ///////////////////////////////
+
   template <typename T>
   static void print(const std::vector<T>& v,
                     bool flag_separate_new_line = false,
